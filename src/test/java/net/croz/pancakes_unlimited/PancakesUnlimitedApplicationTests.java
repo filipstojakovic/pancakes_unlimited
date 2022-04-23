1b1a499a88e5440adcd9d16e3f9cf7b4c9871e11
filipstojakovic <filip.stojakovic1@gmail.com>
2022-04-23 11:06:02 +0200

    init commit

---
+++ src/test/java/net/croz/pancakes_unlimited/PancakesUnlimitedApplicationTests.java
@@ -0,0 +1,13 @@
+package net.croz.pancakes_unlimited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PancakesUnlimitedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
