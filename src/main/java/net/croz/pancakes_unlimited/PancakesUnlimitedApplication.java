b6fc98a02c2018a84ee1c0c56da295afde176657
filipstojakovic <filip.stojakovic1@gmail.com>
2022-04-23 18:22:34 +0200

    init swagger

--- src/main/java/net/croz/pancakes_unlimited/PancakesUnlimitedApplication.java
+++ src/main/java/net/croz/pancakes_unlimited/PancakesUnlimitedApplication.java
@@ -2,6 +2,14 @@ package net.croz.pancakes_unlimited;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class PancakesUnlimitedApplication
@@ -12,4 +20,18 @@ public class PancakesUnlimitedApplication
         SpringApplication.run(PancakesUnlimitedApplication.class, args);
     }
 
+    @Bean
+    public Docket api()
+    {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo()
+    {
+        return new ApiInfo("Pancakes unlimited", "lets bake some pancakes", "1.0.0", "", null, "", "", new ArrayList<>());
+    }
 }
